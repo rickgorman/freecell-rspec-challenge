f7b7f89ae642062c1d587bcecfa99113eb20e50a
rick <gormanite@gmail.com>
2017-09-16 20:08:59 -0400

    Add empty test for Freecell

--- spec/free_cell_spec.rb
+++ spec/free_cell_spec.rb
@@ -27,4 +27,21 @@ describe FreeCell do
     end
 
   end
+
+  describe "#empty?" do
+    
+    context 'when empty' do
+      it 'returns true' do
+        expect(freecell.empty?).to be true
+      end
+    end
+
+    context 'when occupied' do
+      it 'returns false' do
+        freecell.append(hearts2)
+        expect(freecell.empty?).to be false
+      end
+    end
+
+  end
 end
